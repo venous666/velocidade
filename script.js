3b3cdd5d3d9f97b91c25771181c58afd74d46628
venous666 <99412095+venous666@users.noreply.github.com>
2022-09-18 19:09:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+gengar = []
+
+// function formula(){
+//     porra= document.getElementById("gayscombolsonaro")
+//     caralho = document.getElementById("homemdecalcinha")
+//     resultado = document.getElementById("pedrotorquespelado")
+// resultado.value = parseFloat(porra.value)/parseFloat(caralho.value)
+
+// }
+
+// function formula(){
+//     dorian= document.getElementById("gayscombolsonaro")
+//     basil=document.getElementById("homemdecalcinha")
+//     harry=document.getElementById("pedrotorquespelado")
+//     harry.value = parseFloat(dorian.value)/(parseFloat(basil.value)*parseFloat(basil.value))
+//     gengar.push(harry.value)
+//     console.log(harry.value);
+// }
+
+// function limpar(){
+//     document.getElementById("gayscombolsonaro").value=""
+//     document.getElementById("homemdecalcinha").value=""
+// document.getElementById("pedrotorquespelado").value=""
+
+// }
+
+function formula(){
+    gabriela=document.getElementById("gayscombolsonaro")
+    sofia=document.getElementById("homemdecalcinha")
+    venus=document.getElementById("pedrotorquespelado")
+    venus.value=parseFloat(gabriela.value)*parseFloat(sofia.value)
+}
+
+function limpar(){
+    gabriela=document.getElementById("gayscombolsonaro").value=""
+    sofia=document.getElementById("homemdecalcinha").value=""
+    venus=document.getElementById("pedrotorquespelado").value=""
+}

126dd86011d4e8934930147a8c8bca129c9c210c
venous666 <99412095+venous666@users.noreply.github.com>
2022-09-18 19:11:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+gengar = []
+distancia = document.getElementById("distancia")
+tempo = document.getElementById("tempo")
+resultado = document.getElementById("resultado")
+historico = document.getElementById("historico")
+
+
+document.getElementById("limpa") ;{
+    desloc.value = ""
+    tempo.value = ""
+    resultado.value = ""
+}
